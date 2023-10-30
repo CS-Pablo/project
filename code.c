0c27856d3c23878ce9fa1002c9909645ea91fe11
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2023-10-30 16:40:38 +0100

    Update code.c

---
+++ code.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Fonction principale
+int main() {
+    // Instructions du programme
+    printf("Début du programme\n");
+
+    // Déclarations de variables
+    int entier = 42;
+    float virgule = 3.14159;
+    char caractere = 'A';
+
+    // Affichage des variables
+    printf("Entier : %d\n", entier);
+    printf("Virgule : %f\n", virgule);
+    printf("Caractère : %c\n", caractere);
+
+    // Instructions supplémentaires
+
+    // Fin du programme
+    printf("Fin du programme\n");
+
+    return 0;
+}
